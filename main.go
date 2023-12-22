925f19cbe862731e5ce84403c2a2484c5cf9cc19
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2023-12-22 10:52:04 +0900

    add go routine and channel

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 
@@ -27,29 +28,40 @@ func main() {
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-	var results = make(map[string]string) 
+	// var results = make(map[string]string) 
 
-	urls := []string {
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://amazon.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://instagram.com/",
-		"https://academy.nomadcoders.co/",
+	// urls := []string {
+	// 	"https://www.airbnb.com/",
+	// 	"https://www.google.com/",
+	// 	"https://amazon.com/",
+	// 	"https://www.reddit.com/",
+	// 	"https://soundcloud.com/",
+	// 	"https://www.facebook.com/",
+	// 	"https://instagram.com/",
+	// 	"https://academy.nomadcoders.co/",
+	// }
+	// for _,url := range urls{
+	// 	result := "OK"
+	// 	err := hitURL(url)
+	// 	if err != nil {
+	// 		result = "FAILED"
+	// 	}
+	// 	results[url] = result
+	// }
+	// for url, result := range results{
+	// 	fmt.Println(url, result)
+	// }
+	c:= make(chan string)
+	people := [5]string {"A", "B", "C", "D", "E"}
+	for _, person := range people{
+		go isSexy(person, c)
 	}
-	for _,url := range urls{
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+	for i:=0 ; i< len(people); i++{
+		fmt.Println(<-c)
 	}
-	for url, result := range results{
-		fmt.Println(url, result)
-	}
+	// go sexyCount("GJ")
+	// go sexyCount("SH")
+	// time.Sleep(time.Second * 5)
 }
 
 func hitURL(url string) error{
@@ -59,4 +71,16 @@ func hitURL(url string) error{
 		return errRequestFailed
 	}
 	return nil
+}
+
+func sexyCount(person string) {
+	for i:= 0; i <10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func isSexy(person string, c chan string)  {
+	time.Sleep(time.Second * 5)
+	c <- person +  " is sexy"
 }
